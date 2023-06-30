cc1fafe8ffa745643f62a693b363e704f708ec76
Madorskii <madorskiy.017@yandex.ru>
2023-06-30 20:21:06 -0300

    upd

--- bin/exchanger.js
+++ bin/exchanger.js
@@ -6,7 +6,7 @@ import exchange from '../src/index.js';
 program
   .version('1.0.0')
   .description('Text analysis tool that replaces stop words with new values')
-  .arguments('<File> <configName>')
+  .arguments('<fileName> <configName>')
   .helpOption('-h, --help', 'output usage information')
-  .action((File, configName) => exchange(File, configName))
+  .action((file, configName) => exchange(file, configName))
   .parse(process.argv);
