b991942b95613f6c80d4250ea9a494844dd5df91
Madorskii <madorskiy.017@yandex.ru>
2023-07-07 11:44:34 -0300

    lint config upd

--- bin/exchanger.js
+++ bin/exchanger.js
@@ -8,7 +8,5 @@ program
   .description('Text analysis tool that replaces stop words with new values')
   .arguments('<fileName> [configName]')
   .helpOption('-h, --help', 'output usage information')
-  .action((file, configName) =>
-    exchange(file, configName || '__fixtures__/stop-words.yaml')
-  )
+  .action((file, configName) => exchange(file, configName || '__fixtures__/stop-words.yaml'))
   .parse(process.argv);
